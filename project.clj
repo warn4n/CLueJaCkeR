83f1d37f8d8e39fd3ed79a74e5154c9445046e0c
Warren Wood <wwood@pennwell.com>
2009-11-14 23:33:54 -0600

    going to try using leiningen for the build

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject CLueJaCkeR "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.0.0"]
+		 [org.clojure/clojure-contrib "1.0.0"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
+
