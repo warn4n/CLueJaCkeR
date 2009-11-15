a2678c324c9d5ac8b0ed736a327cf66389292136
Warren Wood <wwood@pennwell.com>
2009-11-15 12:01:01 -0600

    wrapper for getting a javax.jcr.Session

---
+++ src/cluejacker/jcr.clj
@@ -0,0 +1,31 @@
+(ns cluejacker.jcr
+  (:import (javax.jcr SimpleCredentials Session Node)
+           (javax.jcr.util TraversingItemVisitor)
+           (org.apache.jackrabbit.rmi.client ClientRepositoryFactory))
+  (:use [clojure.set :only (union)]
+    clojure.contrib.import-static
+    [clojure.contrib.seq-utils :only (includes?)]
+    (clojure.contrib str-utils)))
+
+(defn session [config]
+  (let [{:keys [url user password workspace]} config
+        creds (if user (SimpleCredentials. user (.toCharArray password)))]
+       (System/setProperty "java.rmi.server.useCodebaseOnly" "true")
+       (.. (ClientRepositoryFactory.) (getRepository url) (login creds workspace))))
+
+
+(defmacro connect [instance configs]
+         `(def ~instance (session (~configs (quote ~instance)))))
+
+(comment
+  (def jcr-configs {'local-cq4 {:url "//127.0.0.1:1099/crx" :user "admin" :password "admin" :workspace "live_author"}
+                    'local-publish-cq4 {:url "//127.0.0.1:1099/crx" :user "admin" :password "admin" :workspace "live_publish"}})
+  (connect local-cq4 jcr-configs)
+
+  ; or, just
+
+  (def local-cq4 (session {:url "//127.0.0.1:1099/crx" :user "admin" :password "admin" :workspace "live_author"}))
+
+  (map #(.getPath %) (iterator-seq (.. local-cq4 getRootNode getNodes)))
+
+  )
