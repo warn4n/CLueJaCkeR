4558c94e03434726b9b26047ddee5d61797caf7b
Warren Wood <wwood@pennwell.com>
2009-11-16 16:25:05 -0600

    switched deps to snapshots

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject CLueJaCkeR "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.0.0"]
-		 [org.clojure/clojure-contrib "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [javax.jcr/jcr "1.0"]
-                 [org.apache.jackrabbit/jackrabbit-jcr-rmi "1.4.1"]])
+                 [org.apache.jackrabbit/jackrabbit-jcr-rmi "1.4.1"]
+                 [crx/crx-rmi "1.4.0"]
+                 [org.enclojure/enclojure-repl "1.0"]])
 
