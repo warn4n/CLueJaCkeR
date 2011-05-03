02b97e2abf1580bf63cf7e69a82765f5a33a30fc
Warren Wood <wwood@pennwell.com>
2011-05-03 16:06:40 -0500

    modernizing dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject CLueJaCkeR "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2"]
+                 [org.clojure/clojure-contrib "1.2"]
                  [javax.jcr/jcr "1.0"]
                  [org.apache.jackrabbit/jackrabbit-jcr-rmi "1.4.1"]
-                 [crx/crx-rmi "1.4.0"]
-		 [org.enclojure/enclojure-repl "1.0"]]
-  :dev-dependencies [[com.codestuffs.clojure/swank-clojure "1.0-SNAPSHOT"]])
+                 [crx/crx-rmi "1.4.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 
